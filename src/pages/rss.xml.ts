4c542b33938d7911ee9f11dc5986e721d01a11f5
Pgah <pgah1907@gmail.com>
2026-06-28 02:38:50 +0300

    rss.xml xhtml redirect ile duzelt

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,3 +1,16 @@
 export function GET() {
-  return Response.redirect('https://www.youtube.com/watch?v=ztABYog5x3o', 301);
+  const url = 'https://www.youtube.com/watch?v=ztABYog5x3o';
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script type="text/javascript">//<![CDATA[
+window.location.replace("${url}");
+//]]></script>
+</head>
+<body>
+<p>Redirecting to <a href="${url}">Matrix</a>...</p>
+</body>
+</html>`
+  );
 }
